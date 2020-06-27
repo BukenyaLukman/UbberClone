585c020f51533ae17d4946868bd35db6ed60c130
BukenyaLukman <bukenyalukman@gmail.com>
2020-06-27 13:08:22 +0300

    Setting up Splash Activity

---
+++ app/src/main/java/com/example/ubberclone/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.ubberclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Completable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Action;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        delaySplashScreen();
+    }
+
+    private void delaySplashScreen() {
+        Completable.timer(5, TimeUnit.SECONDS,
+                AndroidSchedulers.mainThread())
+                .subscribe(() -> Toast.makeText(SplashScreenActivity.this, "Splash screen done!!", Toast.LENGTH_SHORT).show());
+    }
+}
